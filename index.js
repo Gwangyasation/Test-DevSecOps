cbfd4103388366a25cf2fb9937518226a7567863
Orakarn Sanwijit <2248orakarn@gmail.com>
2026-03-23 23:43:14 +0700

    Update greeting message in the root route

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('Hello Kitty!'));
+app.get('/', (req, res) => res.send('DevSecOps Lab: Hello Orakarn!'));
 app.listen(3000, () => console.log('Server running on port 3000'));
